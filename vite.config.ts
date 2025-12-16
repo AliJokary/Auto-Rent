0dd89653273528f77728b293ba370a8eccf7a9e0
AliJokari <alijokary2026@gmail.com>
2025-12-16 16:46:00 +0330

    Create Header

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  
+  
   plugins: [
     tailwindcss(),
+    
   ],
   base:"./"
 })
